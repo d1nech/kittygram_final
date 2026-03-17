6f87bf5d76e1de9448cd59245d56f6e60652bb53
Dmitry <necaevdimp@gmail.com>
2026-03-17 09:03:55 +0300

    Commit changes

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,9 +6,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'backend', 'kittyfree.hopto.org']
+
+POSTGRES_USER = os.getenv('POSTGRES_USER', 'django_user')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'mysecretpassword')
+POSTGRES_DB = os.getenv('POSTGRES_DB', 'django')
+DB_HOST = os.getenv('DB_HOST', 'db')
+DB_PORT = os.getenv('DB_PORT', '5432')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -56,8 +62,12 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': POSTGRES_DB,
+        'USER': POSTGRES_USER,
+        'PASSWORD': POSTGRES_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
 
@@ -92,6 +102,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -111,3 +122,10 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://kittyfree.hopto.org',
+    'http://kittyfree.hopto.org',
+    'https://158.160.210.182',
+    'http://158.160.210.182',
+]
